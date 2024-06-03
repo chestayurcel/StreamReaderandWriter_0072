6bf4c170209a91e3d5851ee5e9e7538b627b21e4
Yurcell <chestayurcel@gmail.com>
2024-06-03 14:36:19 +0700

    menambahkan pembacaan file baris demi baris menggunakan ifstream

--- ioFile.cpp
+++ ioFile.cpp
@@ -33,4 +33,12 @@ int main() {
 
     cout << endl << ">= Membuka dan membaca file " << endl;
     // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris)) {
+            // dan tampilkan disini
+            cout << baris << '\n';
+        }
+    }
 }
