d402380807cc9273e3251b9c217eb95302eb9967
Yurcell <chestayurcel@gmail.com>
2024-06-03 14:29:29 +0700

    membuat karakter q agar loop berhenti, dan menulis dan memasukkan nilai dari 'baris' ke dalam file

--- ioFile.cpp
+++ ioFile.cpp
@@ -16,8 +16,11 @@ int main() {
     // unlimited loop untuk menulis
     while (true) {
         cout << "- ";
-        //mendapatkan setiap karakter dalam satu baris
+        // mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
         // loop akan berhenti jika memasukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
     }
 }
