b453186cd97625473611af1bcf57d4f2bbfb9ee2
Yurcell <chestayurcel@gmail.com>
2024-06-03 14:27:49 +0700

    membuat loop untuk menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -12,4 +12,12 @@ int main() {
     outfile.open("contohfile.txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika memasukkan karakter q
+    }
 }
